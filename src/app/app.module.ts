c4dbfb8e177e5c0a7111991164f266e51f4c44ad
Johan <jach_1997@hotmail.com>
2018-10-05 17:29:05 -0500

    Create Login Function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 // FIREBASE
 import {AngularFireModule} from 'angularfire2';
-import {AngularFirestoreModule} from 'angularfire2/firestore';
+// import {AngularFirestoreModule} from 'angularfire2/firestore';
 import {AngularFireAuthModule} from '@angular/fire/auth';
 // COMPONENTS
 import {AppComponent} from './app.component';
@@ -14,6 +14,14 @@ import {FooterComponent} from './components/footer/footer.component';
 import {ContentComponent} from './components/content/content.component';
 import {LoginComponent} from './components/login/login.component';
 import {AdvisoryComponent} from './components/advisory/advisory.component';
+import {HomeComponent} from './components/home/home.component';
+import {RouterModule, Routes} from '@angular/router';
+import {MainComponent} from './components/main/main.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: MainComponent}
+];
 
 @NgModule({
   declarations: [
@@ -22,16 +30,20 @@ import {AdvisoryComponent} from './components/advisory/advisory.component';
     FooterComponent,
     ContentComponent,
     LoginComponent,
-    AdvisoryComponent
+    AdvisoryComponent,
+    HomeComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule,
-    AngularFireAuthModule
+    // AngularFirestoreModule,
+    AngularFireAuthModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase';
+import {AngularFireAuth} from 'angularfire2/auth';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +9,25 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  user: firebase.User;
 
-  constructor() {
+  constructor(
+    private afAuth: AngularFireAuth,
+    private route: ActivatedRoute,
+    private router: Router) {
+    afAuth.authState.subscribe(user => this.user = user);
   }
 
   ngOnInit() {
   }
 
+  login() {
+    this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+    this.router.navigate(['/home']);
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+    this.router.navigate(['']);
+  }
 }
